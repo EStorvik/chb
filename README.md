187ac3539fd1d2e027b27233f7229b3f152a0099
EStorvik <erlend.storvik@hvl.no>
2023-10-31 14:58:06 +0100

    Setup basic package structure

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# ch
-Implementation of Cahn-Hilliard equations and extensions in the Fenics library.
+![build](https://github.com/pmgbergen/DarSIA/workflows/Build%20test/badge.svg)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+[![License: Apache v2](https://img.shields.io/hexpm/l/apa)](https://opensource.org/licenses/Apache-2.0)
+
+# CHB
+Cahn-Hilliard-Biot Simulations
+
+## Installation
+CHB is developed under Python 3.10. Clone the repository from github and enter the chb folder. Then, run the following command to install:
+
+```bash
+pip install .
+```
+To install chb as editable (recommended), along with the tools to develop and run tests, run the following in your virtual environment:
+```bash
+$ pip install -e .[dev]
+```
+
+## Developing CHB
+
+Use black, flake8 and isort formatting.
---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+with open("requirements-dev.txt") as f:
+    required_dev = f.read().splitlines()
+
+setup(
+    name="chb",
+    version="0.0.1",
+    description="Cahn-Hilliard-Biot simulation library",
+    keywords="Cahn-Hilliard flow deformation fenics solvers",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    package_data={"chb": ["py.typed"]},
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python ::3",
+        "Programming Language :: Python ::3.10"
+        "License :: OSI Approved :: GNU GENERAL PUBLIC LICENSE v3 License",
+        "Operating System :: Linux",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=required,
+    extras_require={
+        "dev": required_dev,
+    },
+    python_requires=">=3",
+    url="https://github.com/EStorvik/chb.git",
+    author="Erlend Storvik",
+    maintainer="Erlend Storvik",
+    maintainer_email="erlend.storvik@hvl.no",
+    platforms=["Linux"],
+    license="GNU GPL v2",
+)
