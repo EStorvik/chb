a353f702efc1f2f6f1aaca52ad86678fd0302c28
CedRieth <cedric.riethmueller@mathematik.uni-stuttgart.de>
2025-07-07 13:08:30 +0200

    added new discretization with theta, p and some features

--- computations/chb/chb_monolithic_semi_imp_new_disc.py
+++ computations/chb/chb_monolithic_semi_imp_new_disc.py
@@ -61,19 +61,22 @@ msh = mesh.create_unit_square(MPI.COMM_WORLD, nx, ny, mesh.CellType.triangle)
 ell = 0.1
 gamma = 1
 mobility = 1
-doublewell = chb.energies.DoubleWellPotential()
+doublewell = chb.energies.DoubleWellPotential_cutoff()
 
 # Elasticity
-stiffness_tensor = chb.elasticity.IsotropicStiffnessTensor(
-    lame_lambda_0=20, lame_mu_0=100, lame_lambda_1=0.1, lame_mu_1=1
-)
-swelling = chb.elasticity.Swelling(swelling_parameter=0.1, pf_ref=0.5)
+# isotropic stiffness tensor
+#stiffness_tensor = chb.elasticity.IsotropicStiffnessTensor(
+#    lame_lambda_0=20, lame_mu_0=100, lame_lambda_1=0.1, lame_mu_1=1
+#)
+# heterogeneous and anisotropic stiffness tensor
+stiffness_tensor = chb.elasticity.HeterogeneousStiffnessTensor()
+swelling = chb.elasticity.Swelling(swelling_parameter=0.1, pf_ref=0)
 
 # Biot
 alpha = chb.biot.NonlinearBiotCoupling(alpha0=1, alpha1=0.1)
 
 # Flow
-permeability = chb.flow.NonlinearPermeability(k0=1, k1=0.01)
+permeability = 1
 compressibility = chb.flow.NonlinearCompressibility(M0=1, M1=0.1)
 
 # Time discretization
@@ -89,9 +92,7 @@ tol = 1e-6
 # Finite elements
 P1 = element("Lagrange", msh.basix_cell(), 1)
 P1U = element("Lagrange", msh.basix_cell(), 1, shape=(msh.geometry.dim,))
-P0 = element("DG", msh.basix_cell(), 0)
-Q1 = element("RT", msh.basix_cell(), 1)
-ME = mixed_element([P1, P1, P1U, P0, Q1])
+ME = mixed_element([P1, P1, P1U, P1, P1])
 
 # Function spaces
 V = functionspace(msh, ME)
@@ -99,18 +100,18 @@ V = functionspace(msh, ME)
 # Test and trial functions
 xi = TrialFunction(V)
 eta = TestFunction(V)
-pf, mu, u, p, q = split(xi)
-eta_pf, eta_mu, eta_u, eta_p, eta_q = split(eta)
+pf, mu, u, theta, p = split(xi)
+eta_pf, eta_mu, eta_u, eta_theta, eta_p = split(eta)
 
 
 # Iteration functions
 xi_n = Function(V)
 
 xi_prev = Function(V)
-pf_prev, mu_prev, u_prev, p_prev, q_prev = xi_prev.split()
+pf_prev, mu_prev, u_prev, theta_prev, p_prev = xi_prev.split()
 
 xi_old = Function(V)
-pf_old, mu_old, u_old, p_old, q_old = xi_old.split()
+pf_old, mu_old, u_old, theta_old, p_old = xi_old.split()
 
 
 # Initial condtions
@@ -120,10 +121,10 @@ xi_n.sub(0).interpolate(initialcondition)
 xi_n.sub(1).interpolate(lambda x: np.zeros((1, x.shape[1])))
 xi_n.sub(2).interpolate(lambda x: np.zeros((2, x.shape[1])))
 xi_n.sub(3).interpolate(lambda x: np.zeros((1, x.shape[1])))
-xi_n.sub(4).interpolate(lambda x: np.zeros((2, x.shape[1])))
+xi_n.sub(4).interpolate(lambda x: np.zeros((1, x.shape[1])))
 xi_n.x.scatter_forward()
 
-pf_n, mu_n, u_n, p_n, q_n = xi_n.split()
+pf_n, mu_n, u_n, theta_n, p_n = xi_n.split()
 
 
 # Boundary conditions
@@ -137,25 +138,25 @@ def boundary_right(x):
     return np.isclose(x[0], 1.0)
 
 V_u = V.sub(2)
-V_p = V.sub(3)
+V_theta = V.sub(3)
 facets = mesh.locate_entities_boundary(msh, msh.topology.dim - 1, boundary)
 facets_left = mesh.locate_entities_boundary(msh, msh.topology.dim -1, boundary_left)
 facets_right = mesh.locate_entities_boundary(msh, msh.topology.dim -1, boundary_right)
 dofs_u = locate_dofs_topological(V_u, msh.topology.dim - 1, facets)
-dofs_p_left = locate_dofs_topological(V_p, msh.topology.dim - 1, facets_left)
-dofs_p_right = locate_dofs_topological(V_p, msh.topology.dim - 1, facets_right)
+dofs_theta_left = locate_dofs_topological(V_theta, msh.topology.dim - 1, facets_left)
+dofs_theta_right = locate_dofs_topological(V_theta, msh.topology.dim - 1, facets_right)
 
+_, _, u_bc, theta_bc_left, _ = Function(V).split()
+_, _, _, theta_bc_right, _ = Function(V).split()
 
-_, _, u_bc, p_bc_left, _ = Function(V).split()
-_, _, _, p_bc_right, _ = Function(V).split()
 u_bc.interpolate(lambda x: np.zeros((2, x.shape[1])))
 bc_u = dirichletbc(u_bc, dofs_u)
 
-p_bc_left.interpolate(lambda x: np.ones((1, x.shape[1])))
-p_bc_right.interpolate(lambda x: np.zeros((1, x.shape[1])))
+theta_bc_left.interpolate(lambda x: np.ones((1, x.shape[1])))
+theta_bc_right.interpolate(lambda x: np.zeros((1, x.shape[1])))
 
-bc_p_left = dirichletbc(p_bc_left, dofs_p_left)
-bc_p_right = dirichletbc(p_bc_right, dofs_p_right)
+bc_theta_left = dirichletbc(theta_bc_left, dofs_theta_left)
+bc_theta_right = dirichletbc(theta_bc_right, dofs_theta_right)
 
 # Linear variational forms
 F_pf = (
@@ -165,42 +166,35 @@ F_pf = (
 F_mu = (
     inner(mu, eta_mu) * dx
     - gamma * ell * inner(grad(pf), grad(eta_mu)) * dx
-    - gamma
-    / ell
+    - gamma / ell
     * inner(
         (
-            doublewell.cprime(pf_prev)
-            + doublewell.cdoubleprime(pf_prev) * (pf - pf_prev)
+            doublewell.cprime(pf_prev) + doublewell.cdoubleprime(pf_prev) * (pf - pf_prev)
             - doublewell.eprime(pf_old)
         ),
-        eta_mu,
+        eta_mu
     )
     * dx
     - (
-        0.5
-        * inner(
+        inner(
+            0.5 *
             inner(
-                stiffness_tensor.stress_prime(
-                    strain=sym(grad(u_old)) - swelling(pf_old),
-                    pf=pf_old,
-                ),
-                sym(grad(u_old)) - swelling(pf_old),
+                stiffness_tensor.stress_prime(strain=sym(grad(u_old)) - swelling(pf_old), pf=pf_old),
+                sym(grad(u_old)) - swelling(pf_old)
             )
             - inner(
                 stiffness_tensor.stress(strain=sym(grad(u)) - swelling(pf), pf=pf_old),
-                swelling.prime(),
+                swelling.prime()
             ),
-            eta_mu,
+            eta_mu
         )
     )
     * dx
     - (
         inner(
-            compressibility.prime(pf_old)
-            * p_old**2
-            / (2 * compressibility(pf_old) ** 2)
-            - p_old * alpha.prime(pf_old) * nabla_div(u_old),
-            eta_mu,
+            0.5 * compressibility.prime(pf_old) * (theta_old - alpha(pf_old) * nabla_div(u_old))**2
+            - alpha.prime(pf_old) * compressibility(pf_old) * (theta - alpha(pf_old) * nabla_div(u)) * nabla_div(u_old),
+            eta_mu
         )
         * dx
     )
@@ -209,37 +203,28 @@ F_mu = (
 F_u = (
     inner(
         stiffness_tensor.stress(strain=sym(grad(u)) - swelling(pf), pf=pf_old)
-        - alpha(pf_old) * p * Identity(2),
-        sym(grad(eta_u)),
+        - alpha(pf_old) * compressibility(pf_old) * (theta - alpha(pf_old) * nabla_div(u)) * Identity(2),
+        sym(grad(eta_u))
     )
     * dx
+)
+
+F_theta = (
+    inner(theta - theta_old, eta_theta) * dx + dt * permeability * inner(grad(p), grad(eta_theta)) * dx
 )
 
 F_p = (
-    inner(
-        (
-            p / compressibility(pf_old)
-            + alpha(pf_old) * nabla_div(u)
-            - p_old / compressibility(pf_old)
-            - alpha(pf_old) * nabla_div(u_old)
-        )
-        / dt
-        + nabla_div(q),
-        eta_p,
-    )
-    * dx
+    inner(p, eta_p) * dx - inner(compressibility(pf_old) * (theta - alpha(pf_old) * nabla_div(u)), eta_p) * dx              
 )
 
-F_q = inner(q / permeability(pf_old), eta_q) * dx - inner(p, nabla_div(eta_q)) * dx
-
-F = F_pf + F_mu + F_u + F_p + F_q
+F = F_pf + F_mu + F_u + F_theta + F_p
 
 
 # Set up problem
 a = lhs(F)
 L = rhs(F)
 
-problem = LinearProblem(a, L, bcs=[bc_u, bc_p_left, bc_p_right])
+problem = LinearProblem(a, L, bcs=[bc_u, bc_theta_left, bc_theta_right])
 
 
 # Pyvista plot
@@ -261,27 +246,27 @@ for i in range(num_time_steps):
     # Set old time-step functions
     xi_old.x.array[:] = xi_n.x.array
     xi_old.x.scatter_forward()
-    pf_old, mu_old, u_old, p_old, q_old = xi_old.split()
+    pf_old, mu_old, u_old, theta_old, p_old = xi_old.split()
     # Update current time
     t += dt
 
     for j in range(max_iter):
         xi_prev.x.array[:] = xi_n.x.array
         xi_prev.x.scatter_forward()
-        pf_prev, mu_prev, u_prev, p_old, q_old = (
+        pf_prev, mu_prev, u_prev, theta_prev, p_prev = (
             xi_prev.split()
         )  # This seem only to be necessary for the computation of the L2-norm
 
         # Define the problem
         xi_n = problem.solve()
         xi_n.x.scatter_forward()
-        pf_n, mu_n, u_n, p_old, q_old = (
+        pf_n, mu_n, u_n, theta_n, p_n = (
             xi_n.split()
         )  # This seem only to be necessary for the computation of the L2-norm
 
         increment = chb.util.l2norm(pf_n - pf_prev)
         print(f"Norm at time step {i} iteration {j}: {increment}")
-        # print(f"Norms for pf, mu, u are: {chb.util.l2norm(pf_n-pf_prev)} {chb.util.l2norm(mu_n-mu_prev)}, {chb.util.l2norm(u_n-u_prev)}")
+        # print(f"Norms for pf, mu, u are: {chb.util.l2norm(pf_n-pf_prev)}, {chb.util.l2norm(mu_n-mu_prev)}, {chb.util.l2norm(u_n-u_prev)}")
         viz.update(xi_n.sub(0), t)
         if increment < tol:
             break
@@ -289,7 +274,7 @@ for i in range(num_time_steps):
         # Update the plot window
 
     # Output
-    pf_out, _, _, p_out, _ = xi_n.split()
+    pf_out, _, _, _, p_out = xi_n.split()
     output_file_pf.write_function(pf_out, t)
     output_file_p.write_function(p_out, t)
 
