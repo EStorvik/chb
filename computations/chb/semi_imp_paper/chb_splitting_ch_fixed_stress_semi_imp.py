da6666c4f8570f1e5144d202beca18b7b3fa77ac
EStorvik <erlend.storvik@hvl.no>
2025-10-03 09:40:38 +0200

    Change to symmetric

--- computations/chb/semi_imp_paper/chb_splitting_ch_fixed_stress_semi_imp.py
+++ computations/chb/semi_imp_paper/chb_splitting_ch_fixed_stress_semi_imp.py
@@ -4,6 +4,11 @@ import numpy as np
 
 from mpi4py import MPI
 
+import pandas
+
+from time import time
+
+
 from petsc4py import PETSc
 
 from basix.ufl import element, mixed_element
@@ -55,14 +60,14 @@ from ufl import (
 )
 
 # Spatial discretization
-nx = ny = 32
+nx = ny = 64
 msh = mesh.create_unit_square(MPI.COMM_WORLD, nx, ny, mesh.CellType.triangle)
 
 # Define material parameters
 
 # CH
-ell = 0.1
-gamma = 1
+ell = 0.025
+gamma = 16
 mobility = 1
 doublewell = chb.energies.DoubleWellPotential_cutoff()
 
@@ -73,10 +78,12 @@ doublewell = chb.energies.DoubleWellPotential_cutoff()
 #)
 # heterogeneous and anisotropic stiffness tensor
 stiffness_tensor = chb.elasticity.HeterogeneousStiffnessTensor()
-swelling = chb.elasticity.Swelling(swelling_parameter=0.1, pf_ref=0)
+swelling = chb.elasticity.Swelling(swelling_parameter=0.1, pf_ref=0.5)
 
 # Biot
 alpha = chb.biot.NonlinearBiotCoupling(alpha0=1, alpha1=0.1)
+
+L = 1
 
 # Flow
 permeability = 1
@@ -96,11 +103,12 @@ tol_split = 1e-6
 P1 = element("Lagrange", msh.basix_cell(), 1)
 P1U = element("Lagrange", msh.basix_cell(), 1, shape=(msh.geometry.dim,))
 MEch = mixed_element([P1, P1])
-MEb = mixed_element([P1U, P1, P1])
+MEf = mixed_element([P1, P1])
 
 # Function spaces
 Vch = functionspace(msh, MEch)
-Vb = functionspace(msh, MEb)
+Ve = functionspace(msh, P1U)
+Vf = functionspace(msh, MEf)
 
 # Solution and test functions
 xiCH = Function(Vch)
@@ -108,37 +116,43 @@ etaCH = TestFunction(Vch)
 pf, mu = split(xiCH)
 eta_pf, eta_mu = split(etaCH)
 
-xiB = TrialFunction(Vb)
-etaB = TestFunction(Vb)
-u, theta, p = split(xiB)
-eta_u, eta_theta, eta_p = split(etaB)
-xiB_n = Function(Vb)
+u = TrialFunction(Ve)
+eta_u = TestFunction(Ve)
+u_n = Function(Ve)
+
+xiF = TrialFunction(Vf)
+etaF = TestFunction(Vf)
+theta, p = split(xiF)
+eta_theta, eta_p = split(etaF)
+xiF_n = Function(Vf)
 
 # Solution function at previous time step
 xiCH_old = Function(Vch)
 pf_old, mu_old = split(xiCH_old)
-xiB_old = Function(Vb)
-u_old, theta_old, p_old = split(xiB_old)
+u_old = Function(Ve)
+xiF_old = Function(Vf)
+theta_old, p_old = split(xiF_old)
 
 # Solution function at previous iteration step
 xiCH_prev = Function(Vch)
 pf_prev, mu_prev = split(xiCH_prev)
-xiB_prev = Function(Vb)
-u_prev, theta_prev, p_prev = split(xiB_prev)
+u_prev = Function(Ve)
+xiF_prev = Function(Vf)
+theta_prev, p_prev = split(xiF_prev)
 
 
 # Initial condtions
-initialcondition_cross = chb.initialconditions.Cross(width = 0.3)
+# initialcondition_cross = chb.initialconditions.Cross(width = 0.3)
 initialcondition = chb.initialconditions.halfnhalf
 xiCH.sub(0).interpolate(initialcondition)
 xiCH.sub(1).interpolate(lambda x: np.zeros((1, x.shape[1])))
 xiCH.x.scatter_forward()
 
-xiB_n.sub(0).interpolate(lambda x: np.zeros((2, x.shape[1])))
-xiB_n.sub(1).interpolate(lambda x: np.zeros((1, x.shape[1])))
-xiB_n.sub(2).interpolate(lambda x: np.zeros((1, x.shape[1])))
-xiB_n.x.scatter_forward()
-u_n, theta_n, p_n = split(xiB_n)
+u_n.interpolate(lambda x: np.zeros((2, x.shape[1])))
+xiF_n.sub(0).interpolate(lambda x: np.zeros((1, x.shape[1])))
+xiF_n.sub(1).interpolate(lambda x: np.zeros((1, x.shape[1])))
+xiF_n.x.scatter_forward()
+theta_n, p_n = split(xiF_n)
 
 # Boundary conditions
 def boundary(x):
@@ -150,25 +164,24 @@ def boundary_left(x):
 def boundary_right(x):
     return np.isclose(x[0], 1.0)
 
-V_u = Vb.sub(0)
-V_p = Vb.sub(2)
+# V_p = Vb.sub(2)
 facets = mesh.locate_entities_boundary(msh, msh.topology.dim - 1, boundary)
 #facets_left = mesh.locate_entities_boundary(msh, msh.topology.dim -1, boundary_left)
 #facets_right = mesh.locate_entities_boundary(msh, msh.topology.dim -1, boundary_right)
-dofs_u = locate_dofs_topological(V_u, msh.topology.dim - 1, facets)
-dofs_p = locate_dofs_topological(V_p, msh.topology.dim - 1, facets)
+dofs_u = locate_dofs_topological(Ve, msh.topology.dim - 1, facets)
+# dofs_p = locate_dofs_topological(V_p, msh.topology.dim - 1, facets)
 #dofs_p_left = locate_dofs_topological(V_p, msh.topology.dim - 1, facets_left)
 #dofs_p_right = locate_dofs_topological(V_p, msh.topology.dim - 1, facets_right)
 
-u_bc, _, p_bc = Function(Vb).split()
+u_bc = Function(Ve)
 #u_bc, _, p_bc_left = Function(Vb).split()
 #_, _, p_bc_right = Function(Vb).split()
 
 u_bc.interpolate(lambda x: np.zeros((2, x.shape[1])))
 bc_u = dirichletbc(u_bc, dofs_u)
 
-p_bc.interpolate(lambda x: np.zeros((1, x.shape[1])))
-bc_p = dirichletbc(p_bc, dofs_p)
+# p_bc.interpolate(lambda x: np.zeros((1, x.shape[1])))
+# bc_p = dirichletbc(p_bc, dofs_p)
 
 #p_bc_left.interpolate(lambda x: np.ones((1, x.shape[1])))
 #p_bc_right.interpolate(lambda x: np.zeros((1, x.shape[1])))
@@ -220,30 +233,35 @@ F_mu = (
 F_u = (
     inner(
         stiffness_tensor.stress(strain=sym(grad(u)) - swelling(pf), pf=pf_old)
-        - alpha(pf) * compressibility(pf_old) * (theta - alpha(pf) * div(u)) * Identity(2),
+        - alpha(pf) * compressibility(pf_old) * (theta_n - alpha(pf) * div(u)) * Identity(2),
         sym(grad(eta_u))
     )
     * dx
 )
 
 F_theta = (
-    inner(theta - theta_old, eta_theta) * dx + dt * permeability * inner(grad(p), grad(eta_theta)) * dx
+    inner(theta - theta_old, eta_theta) * dx +L*inner(p-p_prev, eta_theta)*dx + dt * permeability * inner(grad(p), grad(eta_theta)) * dx
 )
 
 F_p = (
-    inner(p, eta_p) * dx - inner(compressibility(pf_old) * (theta - alpha(pf) * div(u)), eta_p) * dx              
-)
+    inner(p, eta_p) * dx  - inner(compressibility(pf_old) * (theta - alpha(pf) * div(u_n)), eta_p) * dx              
+) 
 
 Fch = F_pf + F_mu 
-Fb = F_u + F_theta + F_p
+Ff = F_theta + F_p
 
-
-# Set up non-linear problems
+# Set up non-linear problem
 problemCH = NonlinearProblem(Fch, xiCH, bcs=[])
 
-aB = lhs(Fb)
-lB = rhs(Fb)
-problemB = LinearProblem(aB, lB, bcs=[bc_u, bc_p]) #bcs=[bc_u, bc_p_left, bc_p_right], bcs=[bc_u]
+# Elasticity
+au = lhs(F_u)
+lu = rhs(F_u)
+problemu = LinearProblem(au, lu, bcs=[bc_u]) #bcs=[bc_u, bc_p_left, bc_p_right], bcs=[bc_u]
+
+# Flow
+af = lhs(Ff)
+lf = rhs(Ff)
+problemf = LinearProblem(af, lf, bcs = [])
 
 # Set up Newton solver
 solverCH = NewtonSolver(MPI.COMM_WORLD, problemCH)
@@ -252,12 +270,13 @@ solverCH.rtol = 1e-6
 #solver.convergence_criterion = "incremental"
 
 # Pyvista plot
-viz = chb.visualization.PyvistaVizualization(Vch.sub(0), xiCH.sub(0), 0.0)
+# viz = chb.visualization.PyvistaVizualization(Vch.sub(0), xiCH.sub(0), 0.0)
 #vizP = chb.visualization.PyvistaVizualization(Vb.sub(2), xiB_n.sub(2), 0.0, "pressure")
 
 # Output file
-output_file_pf = XDMFFile(MPI.COMM_WORLD, f"../output/chb_{ell}ell_pf.xdmf", "w")
-output_file_p = XDMFFile(MPI.COMM_WORLD, f"../output/chb_{ell}ell_p.xdmf", "w")
+filenamepath = "../output/chb_splitting_ch_fixed_stress_semi_imp_"
+output_file_pf = XDMFFile(MPI.COMM_WORLD, filenamepath+f"{ell}ell_pf.xdmf", "w")
+output_file_p = XDMFFile(MPI.COMM_WORLD, filenamepath+f"{ell}ell_p.xdmf", "w")
 
 output_file_pf.write_mesh(msh)
 output_file_p.write_mesh(msh)
@@ -281,7 +300,11 @@ def energyTotal(pf, u, theta, dx):
     
 t_vec = []
 energy_vec = []
-
+energy_int_vec = []  # Interface energy
+energy_el_vec = []   # Elastic energy  
+energy_fl_vec = []   # Fluid energy
+iterations = []
+times = []
 # Time stepping
 t = 0.0
 
@@ -289,24 +312,40 @@ for i in range(num_time_steps):
     # Set old time-step functions
     xiCH_old.x.array[:] = xiCH.x.array
     xiCH_old.x.scatter_forward()
-    xiB_old.x.array[:] = xiB_n.x.array
-    xiB_old.x.scatter_forward()
+    u_old.x.array[:] = u_n.x.array
+    u_old.x.scatter_forward()
+    xiF_old.x.array[:] = xiF_n.x.array
+    xiF_old.x.scatter_forward()
 
     # Update current time
     t += dt
-
+    iteration = 0
+    tpre = time()
     for j in range(max_iter_split):
+        iteration += 1
         # Set previous iteration functions
         xiCH_prev.x.array[:] = xiCH.x.array
         xiCH_prev.x.scatter_forward()
-        xiB_prev.x.array[:] = xiB_n.x.array
-        xiB_prev.x.scatter_forward()
-    
+        u_prev.x.array[:] = u_n.x.array
+        u_prev.x.scatter_forward()
+        xiF_prev.x.array[:] = xiF_n.x.array
+        xiF_prev.x.scatter_forward()
         # Solve the non-linear problems
         nCH, convergedCH = solverCH.solve(xiCH)
-        xiB_n = problemB.solve()
-        xiB_n.x.scatter_forward()
-        u_n, theta_n, p_n = xiB_n.split()
+
+        for k in range(max_iter_split):
+            u_prev.x.array[:] = u_n.x.array
+            u_prev.x.scatter_forward()
+            xiF_prev.x.array[:] = xiF_n.x.array
+            xiF_prev.x.scatter_forward()
+            theta_prev, p_prev = xiF_prev.split()
+            u_n = problemu.solve()
+            xiF_n = problemf.solve()
+            theta_n, p_n = xiF_n.split()
+            increment_fs = chb.util.l2norm_2(u_n-u_prev, p_n-p_prev)
+            print(f"Fixed_stress iteration increment error: {increment_fs}")
+            if increment_fs < tol_split:
+                break
         
         increment_split = chb.util.l2norm_3(pf - pf_prev, u_n - u_prev, p_n - p_prev)
         print(f"Increment norm at time step {i} splitting step {j}: {increment_split}")
@@ -314,54 +353,134 @@ for i in range(num_time_steps):
         if increment_split < tol_split:
             break      
         
+    tpost = time()-tpre
     # Update the plot window
-    viz.update(xiCH.sub(0), t)
+    # viz.update(xiCH.sub(0), t)
     #vizP.update(xiB_n.sub(2), t)
     
     energy_total = energyTotal(pf, u_n, theta_n, dx = Measure("dx", domain=msh))        
     energy = assemble_scalar(form(energy_total))
-    print(f"Energy at time step {i}: {energy}")
     
-    t_vec.append(t)
+    # Calculate individual energy components
+    energy_int_form = energy_i(pf, dx = Measure("dx", domain=msh))
+    energy_el_form = energy_e(pf, u_n, dx = Measure("dx", domain=msh))
+    energy_fl_form = energy_f(pf, u_n, theta_n, dx = Measure("dx", domain=msh))
+    
+    energy_int = assemble_scalar(form(energy_int_form))
+    energy_el = assemble_scalar(form(energy_el_form))
+    energy_fl = assemble_scalar(form(energy_fl_form))
+    
+    
+    t_vec.append(tpost)
     energy_vec.append(energy)
+    energy_int_vec.append(energy_int)
+    energy_el_vec.append(energy_el)
+    energy_fl_vec.append(energy_fl)
+    iterations.append(iteration)
+    times.append(tpost)
 
     # Output
     pf_out, _ = xiCH.split()
     output_file_pf.write_function(pf_out, t)
-    _, _, p_out = xiB_n.split()
+    _, p_out = xiF_n.split()
     output_file_p.write_function(p_out, t)
 
 
-viz.final_plot(xiCH.sub(0))
+# viz.final_plot(xiCH.sub(0))
 #vizP.final_plot(xiB_n.sub(2))
 
-plt.figure()
-plt.plot(t_vec, energy_vec, label=f"Total energy")
-plt.show()
+# Create log DataFrame and save to Excel
+log_data = {
+    'Time_Step': range(1, len(t_vec) + 1),  # Enumerate time steps starting from 1
+    'Iterations': iterations,
+    'Time': t_vec,
+    'Total_Energy': energy_vec,
+    'Interface_Energy': energy_int_vec,
+    'Elastic_Energy': energy_el_vec,
+    'Fluid_Energy': energy_fl_vec
+}
 
-def plot_along_line(u, msh, y=0.5, filename="line_data.npy"):
-    # Create an array of x-coordinates along the line y=0.5
-    x_coords = np.linspace(msh.geometry.x.min(), msh.geometry.x.max(), 100)
-    y_coord = y
-    points = np.array([[x, y_coord, 0] for x in x_coords])
+log_df = pandas.DataFrame(log_data)
+log_filenamepath = "../output/log/chb_splitting_ch_fixedstress_semi_imp.xlsx"
 
-    tree = bb_tree(msh, msh.geometry.dim)
-    values = []
+# Create directory if it doesn't exist
+import os
+os.makedirs(os.path.dirname(log_filenamepath), exist_ok=True)
 
-    for point in points:
-        cell_candidates = compute_collisions_points(tree, point.T)
-        cell = compute_colliding_cells(msh, cell_candidates, point).array
-        assert len(cell) > 0
-        first_cell = cell[0]
-        values.append(u.eval(point, first_cell))
+try:
+    # Try to save as Excel file (requires openpyxl)
+    log_df.to_excel(log_filenamepath, index=False, sheet_name='Simulation_Log')
+    print(f"Log data saved to Excel file: {log_filenamepath}")
+except ImportError:
+    # Fallback to CSV if openpyxl is not available
+    csv_path = log_filenamepath.replace('.xlsx', '.csv')
+    log_df.to_csv(csv_path, index=False)
+    print(f"Excel writer not available, log data saved to CSV: {csv_path}")
+    print("Install openpyxl with: pip install openpyxl")
 
-    # Save the x-coordinates and values to a numpy file
-    np.save(filename, {"x_coords": x_coords, "values": values})
+# Plot Total Energy
+# plt.figure(figsize=(10, 6))
+# plt.plot(t_vec, energy_vec, 'r-', linewidth=2, label="Total energy")
+# plt.xlabel('Time')
+# plt.ylabel('Total Energy')
+# plt.title('Total Energy Evolution')
+# plt.legend()
+# plt.grid(True, alpha=0.3)
+# plt.show()
 
-    plt.figure()
-    plt.plot(x_coords, values, label=f"Solution at y={0.5}")
+# Plot Interface Energy
+# plt.figure(figsize=(10, 6))
+# plt.plot(t_vec, energy_int_vec, 'b-', linewidth=2, label="Interface energy")
+# plt.xlabel('Time')
+# plt.ylabel('Interface Energy')
+# plt.title('Interface Energy Evolution')
+# plt.legend()
+# plt.grid(True, alpha=0.3)
+# plt.show()
 
-    plt.show()
+# Plot Elastic Energy
+# plt.figure(figsize=(10, 6))
+# plt.plot(t_vec, energy_el_vec, 'g-', linewidth=2, label="Elastic energy")
+# plt.xlabel('Time')
+# plt.ylabel('Elastic Energy')
+# plt.title('Elastic Energy Evolution')
+# plt.legend()
+# plt.grid(True, alpha=0.3)
+# plt.show()
+
+# Plot Fluid Energy
+# plt.figure(figsize=(10, 6))
+# plt.plot(t_vec, energy_fl_vec, 'm-', linewidth=2, label="Fluid energy")
+# plt.xlabel('Time')
+# plt.ylabel('Fluid Energy')
+# plt.title('Fluid Energy Evolution')
+# plt.legend()
+# plt.grid(True, alpha=0.3)
+# plt.show()
+
+# def plot_along_line(u, msh, y=0.5, filename="line_data.npy"):
+#     # Create an array of x-coordinates along the line y=0.5
+#     x_coords = np.linspace(msh.geometry.x.min(), msh.geometry.x.max(), 100)
+#     y_coord = y
+#     points = np.array([[x, y_coord, 0] for x in x_coords])
+
+#     tree = bb_tree(msh, msh.geometry.dim)
+#     values = []
+
+#     for point in points:
+#         cell_candidates = compute_collisions_points(tree, point.T)
+#         cell = compute_colliding_cells(msh, cell_candidates, point).array
+#         assert len(cell) > 0
+#         first_cell = cell[0]
+#         values.append(u.eval(point, first_cell))
+
+#     # Save the x-coordinates and values to a numpy file
+#     np.save(filename, {"x_coords": x_coords, "values": values})
+
+#     plt.figure()
+#     plt.plot(x_coords, values, label=f"Solution at y={0.5}")
+
+#     plt.show()
 
 #plot_along_line(xiCH.sub(0), msh=msh, filename=f"../output/line_data_{ell}ell.npy")
 #plot_along_line(xiB_n.sub(2), msh=msh, filename=f"../output/line_data_{ell}ell.npy")
